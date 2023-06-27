053fb621655cef647978adbdf4695e9b4a8b88ac
Kirill Pravdin <kirill-pravdin@mail.ru>
2023-06-27 23:35:08 +0300

    feat: add sass

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import style from './Projects.module.css'
+import style from './Projects.module.scss'
 import styleContainer from '../common/styles/Container.module.css'
 import Project from './project/Project';
 import {Title} from '../common/components/title/Title';
 import todoImage from '../assets/image/todoImage.jpg'
-import socialImage from '../assets/image/socialImage.jpg'
+import socialImage from '../assets/image/socialImage.png'
 
 const Projects = () => {
-    const social = {
-        backgroundImage: 'url(' + socialImage + ')',
-    }
-    const todo = {
-        backgroundImage: 'url(' + todoImage + ')',
-    }
+    const social = {backgroundImage: 'url(' + socialImage + ')'}
+    const todo = {backgroundImage: 'url(' + todoImage + ')'}
     return (
         <div className={style.projectsBlock}>
             <div className={`${styleContainer.container} ${style.projectsContainer}`}>
                 <Title title={"Projects"}/>
                 <div className={style.projects}>
-                    <Project style={todo}  title={"Todo list"} description={"Simple task management service"}/>
-                    <Project style={social}  title={"Social network"} description={"An online platform that is used for communication, dating, creating social relationships between people who have similar interests or offline connections, as well as for entertainment (music, movies) and work."}/>
+                    <Project path={'https://kiryl15.github.io/todolist-2.0/'} style={todo}  title={"Todo list"} description={"Simple task management service"}/>
+                    <Project path={'https://kiryl15.github.io/samurai-way/'} style={social}  title={"Social network"} description={"An online platform that is used for communication, dating, creating social relationships between people who have similar interests or offline connections, as well as for entertainment (music, movies) and work."}/>
                 </div>
             </div>
         </div>
--- src/projects/project/Project.js
+++ src/projects/project/Project.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import style from './Project.module.css'
+import style from './Project.module.scss'
 
 const Project = (props) => {
     return (
         <div className={style.project}>
             <div className={style.imgContainer} style={props.style}>
-                <a className={style.button} >ПОСМОТРЕТЬ</a>
+                <a href={props.path} className={style.button} >ПОСМОТРЕТЬ</a>
             </div>
             <div className={style.projectInfo}>
                 <h3 className={style.projectTitle}>{props.title}</h3>
                 <span className={style.description}>{props.description}</span>
             </div>
-
         </div>
     );
 };
